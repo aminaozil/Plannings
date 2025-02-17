789149b9b7240d7ff92a6f55a8eb6670130d9a82
aminaozil <youmaminatambengue@gmail.com>
2025-02-17 17:33:06 +0000

    deploiement

--- planning/settings.py
+++ planning/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['plannings-bilingue-tech-avenir.onrender.com']
 
 #pour l'authentification
 AUTH_USER_MODEL = 'utilisateur.User'
