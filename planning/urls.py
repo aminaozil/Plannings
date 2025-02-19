40155eea0ec6c9fa983059f154a850bcffca8806
aminaozil <youmaminatambengue@gmail.com>
2025-02-19 17:51:52 +0000

    modification des image

--- planning/urls.py
+++ planning/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 
@@ -8,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("utilisateur.urls")),
     path('', include("plannings.urls")),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
+urlpatterns += staticfiles_urlpatterns()
