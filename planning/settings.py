d1015d8e94e4f42201ee4d3218b2841bc021a5c6
aminaozil <youmaminatambengue@gmail.com>
2025-02-19 16:20:49 +0000

    modif

--- planning/settings.py
+++ planning/settings.py
@@ -143,7 +143,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/images/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
     ]
