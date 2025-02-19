a91fc9ebf9dcbc75876ad1d15ad1a8ebd5fe44e1
aminaozil <youmaminatambengue@gmail.com>
2025-02-19 16:55:30 +0000

    images

--- planning/urls.py
+++ planning/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +8,4 @@ urlpatterns = [
     path('', include("plannings.urls")),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+
