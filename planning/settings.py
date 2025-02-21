9aa69a594e04fd0b3feba5a68aa320916bf7cb04
aminaozil <youmaminatambengue@gmail.com>
2025-02-21 16:38:06 +0000

    modifs

--- planning/settings.py
+++ planning/settings.py
@@ -147,7 +147,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'templates/static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
