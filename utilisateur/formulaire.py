1974d447f8aa90d3023b11ace3736e5007c44d70
aminaozil <youmaminatambengue@gmail.com>
2025-03-09 14:41:08 +0000

    responsive login and home

--- utilisateur/formulaire.py
+++ utilisateur/formulaire.py
@@ -6,12 +6,12 @@ from plannings.models import Classe, Planning
 class LoginForm(forms.Form):
     email = forms.EmailField(max_length=63, label='Email', widget=forms.EmailInput(
         attrs={
-             "class":"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-emerald-900 dark:focus:border-cyan-500"
+             "class":"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#095415]  focus:border-[#095415]  block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-[#095415]  dark:focus:border-[#095415]"
         }
         ))
     password = forms.CharField(max_length=63,  label='Mot de passe', widget=forms.PasswordInput(
         attrs={
-             "class":"mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-emerald-900 dark:focus:border-cyan-500"
+             "class":"mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#095415]  focus:border-[#095415]  block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-[#095415]  dark:focus:border-[#095415]"
         }
         ))
     
@@ -19,7 +19,7 @@ class LoginForm(forms.Form):
 class FormateurForm(forms.ModelForm):
     nom = forms.CharField(max_length=200, widget=forms.TextInput(
         attrs={
-             "class":"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-cyan-500 dark:focus:border-cyan-500"
+             "class":"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#095415]  focus:border-[#095415]  block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-[#095415]  dark:focus:border-[#095415]"
         }
         ))
     prenom = forms.CharField(max_length=200, widget=forms.TextInput(
